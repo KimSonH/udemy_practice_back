c96ddc656ebce480973309310ce8cb9a5256c589
KimSonH <kimson199816@gmail.com>
2025-11-28 21:50:25 +0700

    fix: update errorUrl and cancelUrl to point to course details instead of payment session

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -299,8 +299,8 @@ export class PaymentsService {
       expiresIn: `${this.configService.get('JWT_VERIFICATION_TOKEN_EXPIRATION_TIME')}s`,
     });
     const successUrl = `${this.configService.get('SITE_URL')}/payments-session?sessionId=${token}`;
-    const errorUrl = `${this.configService.get('SITE_URL')}/payments-session?sessionId=${token}`;
-    const cancelUrl = `${this.configService.get('SITE_URL')}/payments-session?sessionId=${token}`;
+    const errorUrl = `${this.configService.get('SITE_URL')}/courses/${course.id}`;
+    const cancelUrl = `${this.configService.get('SITE_URL')}/courses/${course.id}`;
     const checkoutPayload = this.sepayService.initOneTimePaymentFields({
       invoiceNumber,
       amount: amount * 26000,
